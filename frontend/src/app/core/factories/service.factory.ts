137f894f5b614e347c80fe37242f2c0c3d84b3ea
ZAINAB <zainabsbeity2003@gmail.com>
2026-01-25 00:38:41 +0200

    utilizing factor design

--- frontend/src/app/core/factories/service.factory.ts
+++ frontend/src/app/core/factories/service.factory.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../../users.service';
 
-// Base service interface
 export interface IApiService {
-  getUsers(): Observable<any[]>;
-  createUser(userData: any): Observable<any>;
+  getUsers(): Observable<User[]>;
+  createUser(userData: {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    username: string;
+    password: string;
+  }): Observable<User>;
 }
 
-// Factory interface
 export interface IServiceFactory {
   createApiService(): IApiService;
 }
 
-// Concrete implementation
 @Injectable({
   providedIn: 'root'
 })
@@ -22,23 +26,33 @@ export class UsersApiService implements IApiService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.API_URL}/list`);
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.API_URL}/list`);
   }
 
-  createUser(userData: any): Observable<any> {
-    return this.http.post<any>(`${this.API_URL}/create`, userData);
+  createUser(userData: {
+    firstName: string;
+    lastName: string;
+    dateOfBirth: string;
+    username: string;
+    password: string;
+  }): Observable<User> {
+    return this.http.post<User>(`${this.API_URL}/create`, userData);
   }
 }
 
-// Factory implementation
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceFactory implements IServiceFactory {
+  private apiService: IApiService | null = null;
+
   constructor(private http: HttpClient) {}
 
   createApiService(): IApiService {
-    return new UsersApiService(this.http);
+    if (!this.apiService) {
+      this.apiService = new UsersApiService(this.http);
+    }
+    return this.apiService;
   }
 }
--- frontend/src/app/store/users/users.effects.ts
+++ frontend/src/app/store/users/users.effects.ts
@@ -3,7 +3,7 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, catchError, switchMap, tap, delay } from 'rxjs/operators';
 import { UsersActions } from './users.actions';
-import { UsersService } from '../../users.service';
+import { ServiceFactory, IApiService } from '../../core/factories/service.factory';
 import { NotificationService } from '../../core/services/notification.service';
 
 @Injectable()
@@ -11,17 +11,20 @@ export class UsersEffects {
   loadUsers$: any;
   createUser$: any;
   createUserSuccessNotification$: any;
+  private apiService: IApiService;
 
   constructor(
     private actions$: Actions,
-    private usersService: UsersService,
+    private serviceFactory: ServiceFactory,  
     private notificationService: NotificationService
   ) {
+    this.apiService = this.serviceFactory.createApiService();
+
     this.loadUsers$ = createEffect(() =>
       this.actions$.pipe(
         ofType(UsersActions.loadUsers),
         switchMap(() =>
-          this.usersService.getUsers().pipe(
+          this.apiService.getUsers().pipe(  
             delay(1500), 
             map((users) => UsersActions.loadUsersSuccess({ users })),
             catchError((error) =>
@@ -36,7 +39,7 @@ export class UsersEffects {
       this.actions$.pipe(
         ofType(UsersActions.createUser),
         switchMap(({ userData }) =>
-          this.usersService.createUser(userData).pipe(
+          this.apiService.createUser(userData).pipe(  
             delay(1500),
             map((user) => UsersActions.createUserSuccess({ user })),
             catchError((error) =>
